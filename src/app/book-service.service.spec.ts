fee1d7f55c00befe512c13214209a8e26b16f73e
Athiya Sultana <athiya@olivetech.net>
2018-03-14 11:11:30 +0530

    changes in service

--- src/app/book-service.service.spec.ts
+++ src/app/book-service.service.spec.ts
@@ -2,13 +2,12 @@ import {inject, TestBed} from '@angular/core/testing';
 
 import {BookServiceService} from './book-service.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {HttpClient} from '@angular/common/http';
 import {HttpClientModule} from '@angular/common/http';
 
 describe('BookServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule, HttpClientModule],
+      imports: [HttpClientTestingModule, HttpClientModule],
       providers: [BookServiceService]
     });
   });
@@ -16,4 +15,10 @@ describe('BookServiceService', () => {
   it('should be created', inject([BookServiceService], (service: BookServiceService) => {
     expect(service).toBeTruthy();
   }));
+  describe('GetAllBooks', () => {
+    it('getAllBooks', inject([BookServiceService, HttpTestingController],
+      (bookService: BookServiceService, http: HttpTestingController) => {
+
+      });
+  });
 });
